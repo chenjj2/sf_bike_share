b52aff50042caea87af30cfa57ea0de5a6470d37
Xiaoqi Lu <lx2170@cumc.columbia.edu>
2019-08-02 22:00:47 -0700

    finalize project
    delete trip time
    write up readme

--- README.md
+++ README.md
@@ -8,7 +8,36 @@ Predict the time a trip will take,
 given the start and end bike station.
 
 ## Method
-TBD
+
+#### Data Inclusion Criteria
+We filtered out instances that satisfies one of the following:
+1. trips with same start and end station
+2. trips across cities
+3. mean velocity [*] below 5km/h or higher than 30km/h
+
+[*] mean velocity here is defined as the L2-distance divided by duration.
+
+#### Sub-Sampling
+We sampled 10,000 instances from the original data for training and testing (9 : 1).
+
+#### Feature Engineering
+The following features are included/constructed:
+1. day of week
+2. subscription type
+3. latitude/longitude of start and end
+4. L2-distance between start and end
+5. city
+6. if it is peak hour (8, 9, 16, 17, 18)
+7. mean temperature, dew point, humidity, pressure, visibility, wind speed
+8. maximum gust speed
+9. precipitation
+10. cloud cover
+
+#### Model
+We tried a variety of methods in the sklearn package, including linear regression, LASSO, ridge, gradient boosting, and neural network.
+Then we fine tuned parameters on several models.
 
 ## Performance
-TBD
+The best neural network structure is with (50, 20, 3)-RELU hidden layers, giving R^2 = 0.61 and MSE = 32321 (about 3 min error).
+
+The best gradient boosting regression is with maximum depth = 3, giving R^2 = 0.69 and MSE = 25500 (2 min and 40s error).
--- training/linear_regression.py
+++ training/linear_regression.py
@@ -9,6 +9,8 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import VotingRegressor
+from sklearn.model_selection import GridSearchCV
 
 from preprocess import feature_engineering as fe
 
@@ -16,9 +18,12 @@ INPUT_FILE = '~/Data/sf_bike_share/subsample.csv'
 INPUT_FILE = '../data/subsample.csv'
 METHODS = {'LR': LinearRegression(),
            #'ARD': ARDRegression(),
-           'BayesianRidge': BayesianRidge(),
-           'ElasticNet': ElasticNetCV(), 'Lasso': LarsCV(), 'Lars': LarsCV(), 'Ridge': RidgeCV(),
-           "SGD": SGDRegressor(), "Huber": HuberRegressor(), "Tree": DecisionTreeRegressor(),
+           #'BayesianRidge': BayesianRidge(),
+           'ElasticNet': ElasticNetCV(), 'Lasso': LarsCV(),
+           #'Lars': LarsCV(),
+           'Ridge': RidgeCV(),
+           #"SGD": SGDRegressor(), "Huber": HuberRegressor(),
+           "Tree": DecisionTreeRegressor(),
            #"GP": GaussianProcessRegressor(),
            "GB": GradientBoostingRegressor(), "MLP": MLPRegressor()}
 
@@ -33,7 +38,8 @@ def get_features():
     df = pd.concat([df, fe.one_hot(df, 'zip_code')], axis=1)
     df = pd.concat([df, fe.day_of_week(df, 'start_date')], axis=1)
     df['peak_hour'] = fe.peak_hour(df, 'start_date')
-    df.drop(['lat_x', 'lat_y', 'long_x', 'long_y', 'subscription_type', 'zip_code', 'start_date', 'id'], axis=1, inplace=True)
+    df.drop(['subscription_type', 'zip_code', 'start_date', 'id'], axis=1, inplace=True)
+    df = df[(5 <= df['distance'] / df['duration'] * 3600) & (df['distance'] / df['duration'] * 3600 <= 30)]
     df = df.fillna(df.mean())
 
     # X, y
@@ -46,7 +52,40 @@ def get_features():
 def train(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
+
+    """
+    ereg = VotingRegressor(estimators=[(name, method) for name,method in METHODS.items()])
+    ereg = ereg.fit(X_train, y_train)
+    y_pred = ereg.predict(X_test)
+    """
+
+    """
+    ereg = MLPRegressor(max_iter=1000, early_stopping=True)
+    param_grid = {'hidden_layer_sizes': [(50, 10, 5), (50, 5, 5),
+                                         (50, 20, 3), (50, 20, 5)]}
+    grid_search = GridSearchCV(ereg, param_grid=param_grid, cv=5, iid=False, n_jobs=-1)
+    grid_search.fit(X_train, y_train)
+    y_pred = grid_search.predict(X_test)
+    """
+
+    ereg = GradientBoostingRegressor(n_estimators=500)
+    param_grid = {'max_depth': [3, 4, 5, 6]}
+    grid_search = GridSearchCV(ereg, param_grid=param_grid, cv=5, iid=False, n_jobs=-1)
+    grid_search.fit(X_train, y_train)
+    y_pred = grid_search.predict(X_test)
+
+    print(f"{grid_search.best_params_}")
+    r2 = r2_score(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    # msle = mean_squared_log_error(y_test, y_pred)
+    print([(true, pred) for true, pred in zip(y_test[:5], y_pred[:5])])
+    print(f"r2 {r2}, mse {mse}")
+    df_fit = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred})
+    df_fit.to_csv('../fit/' + 'voting' + '.csv', index=False)
+
+    """
     for name, model in METHODS.items():
+        print('='*30)
         print(f"Model {name}")
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
@@ -54,7 +93,11 @@ def train(X, y):
         r2 = r2_score(y_test, y_pred)
         mse = mean_squared_error(y_test, y_pred)
         #msle = mean_squared_log_error(y_test, y_pred)
+        print([(true,pred) for true,pred in zip(y_test[:5], y_pred[:5])])
         print(f"r2 {r2}, mse {mse}")
+        df_fit = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred})
+        df_fit.to_csv('../fit/' + name + '.csv', index=False)
+    """
 
 if __name__ == '__main__':
     train(*get_features())
