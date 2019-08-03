ede0bf7c064f5843a93296304fe37eea44c1a6e3
Jingjing Chen <chenjj235@gmail.com>
2019-08-02 17:17:20 -0700

    Update linear_regression.py

--- training/linear_regression.py
+++ training/linear_regression.py
@@ -1,45 +1,46 @@
+import pandas as pd
+
 from sklearn.metrics import r2_score, mean_squared_error, mean_squared_log_error
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LassoCV
+from sklearn.linear_model import ARDRegression, BayesianRidge, \
+    ElasticNetCV, LassoCV, LarsCV, RidgeCV, \
+    SGDRegressor, HuberRegressor, LinearRegression
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
 
-import pandas as pd
-from preprocess.trip_distance import cal_trip_distance
-from preprocess.trip_time import trip_id_to_encoded_time
+INPUT_FILE = '~/Data/sf_bike_share/subsample.csv'
+METHODS = {'LR': LinearRegression(),
+           'ARD': ARDRegression(), 'BayesianRidge': BayesianRidge(),
+           'ElasticNet': ElasticNetCV(), 'Lasso': LarsCV(), 'Lars': LarsCV(), 'Ridge': RidgeCV(),
+           "SGD": SGDRegressor(), "Huber": HuberRegressor(), "Tree": DecisionTreeRegressor(),
+           "GP": GaussianProcessRegressor(), "GB": GradientBoostingRegressor(), "MLP": MLPRegressor()}
 
-DATA_PATH = '../data/'
-FILENAME_TRIP = 'trip.csv.zip'
-FILENAME_STATION = 'station.csv'
-FILENAME_COMBINE = 'combine.csv'
+def get_features():
+    df = pd.read_csv(INPUT_FILE)
 
-file_trip = DATA_PATH + FILENAME_TRIP
-file_station = DATA_PATH + FILENAME_STATION
+    # encode
 
-NROWS = None
+    # X, y
+    column_names = list(df.columns)
+    duration_index = column_names.index('duration')
+    X_names = column_names[:duration_index] + column_names[(duration_index + 1):]
 
-df = pd.read_csv(file_trip, usecols=['id', 'duration', 'start_station_id', 'end_station_id'], nrows=NROWS)
-df_time = trip_id_to_encoded_time(file_trip, nrows=NROWS)
-df_distance = cal_trip_distance(file_trip, file_station, NROWS)
+    return X,y
 
-df = df[df['start_station_id'] != df['end_station_id']]
-df = df.merge(df_time, how='left', on='id')
-df = df.merge(df_distance, how='left', on='id')
-df = df.sample(n=10000)
-df.to_csv(DATA_PATH + FILENAME_COMBINE, index=False)
-del df_time, df_distance
+def train(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(df[X_names], df['duration'], test_size=0.1, random_state=42)
 
-df.drop(['id', 'start_station_id', 'end_station_id'], axis=1, inplace=True)
-column_names = list(df.columns)
-duration_index = column_names.index('duration')
-X_names = column_names[:duration_index] + column_names[(duration_index + 1):]
+    for name, model in METHODS.items():
+        print(f"Model {name}")
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        print(f"{model.get_params()}")
+        r2 = r2_score(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        msle = mean_squared_log_error(y_test, y_pred)
+        print(f"r2-{r2}, mse-{mse}, msle-{msle}")
 
-X_train, X_test, y_train, y_test = train_test_split(df[X_names], df['duration'], test_size=0.1, random_state=42)
-
-model = LassoCV()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-print(model.coef_)
-
-r2 = r2_score(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-msle = mean_squared_log_error(y_test, y_pred)
-print(r2, mse, msle)
+if __name__ == '__main__':
+    train(get_features())
